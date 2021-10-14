93da4522b55ec707923511c2f529fd772ce2537e
may <maythinkhaingminoz@gmail.com>
2021-10-14 17:10:10 +0630

    First Changes

--- php_basics/php_basics_data_types.php
+++ php_basics/php_basics_data_types.php
@@ -1,3 +1,26 @@
 <?php
-echo "Testing Data Types" . "</br>"; 
-echo "boolean";
+echo "Testing Data Types\n"; 
+echo "Scalar DataTypes \n";
+echo "\n+++++++\n";
+echo " boolean \n";
+echo "+++++++\n";
+if (TRUE)
+{
+echo "Condition is TRUE";
+}
+else 
+{
+    echo "Condition is FALSE";
+}
+echo "\n\n+++++++\n";
+echo " integer \n";
+echo "+++++++\n";
+$dec_int = 12;  
+$oct_int = 012;  
+$hexa_int = 0x12; 
+echo "Decimal number: " .$dec_int. "\n";  
+echo "Octal number: " .$oct_int. "\n";  
+echo "HexaDecimal number: " .$hexa_int . "\n"; 
+echo "\n+++++++\n";
+echo " Object";
+echo "\n+++++++\n";
